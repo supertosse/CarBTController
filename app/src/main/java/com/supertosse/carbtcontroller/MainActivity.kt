7722b114011275cd8d98f052da0e9a3283e26f3b
Stian Tøsse <supertosse@hotmail.com>
2018-12-06 11:37:08 +0100

    GG

--- app/src/main/java/com/supertosse/carbtcontroller/MainActivity.kt
+++ app/src/main/java/com/supertosse/carbtcontroller/MainActivity.kt
@@ -9,8 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var speed:Int = 0
-    private var turn:Int = 0
+    private var speed:Int = 0   // max is 20
+    private var turn:Int = 0    // Value from 0 to 20(max seekBar value is 20). 10 is straight ahead, 0 is full left.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //Updates speed and turn fields when seekBar is updated
     private fun setupSeekBars(m: MainActivity) {
         speedSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 m.speed = i
+                speedTextView.text = "Speed: " + m.speed.toString()
 
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
@@ -31,15 +33,17 @@ class MainActivity : AppCompatActivity() {
         turnSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 m.turn = i
+                turnTextView.text = "Turn: " + m.turn.toString()
 
             }
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
         })
-
+        speedSeekBar.progress = 10
+        turnSeekBar.progress = 10
     }
 
-
+    // OnClik horn button
     fun honk(view: View){
 
     }
